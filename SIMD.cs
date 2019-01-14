1c758e2ed3ea6951786581bf379303083bf9dc0a
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-01-14 12:58:01 +0100

    Formatting

--- SIMD.cs
+++ SIMD.cs
@@ -3,40 +3,40 @@ using System.Numerics;
 using System.Runtime.CompilerServices;
 
 class SIMD
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Add(ref int[] a, ref int[] b, out int[] result)
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Add(ref int[] a, ref int[] b, out int[] result)
+        result = new int[a.Length];
+        var simdLength = Vector<int>.Count;
+        int i;
+        for (i = 0; i <= a.Length - simdLength; i += simdLength)
         {
-            result = new int[a.Length];
-            var simdLength = Vector<int>.Count;
-            int i;
-            for (i = 0; i <= a.Length - simdLength; i += simdLength)
-            {
-                var va = new Vector<int>(a, i);
-                var vb = new Vector<int>(b, i);
-                (va + vb).CopyTo(result, i);
-            }
-            for (; i < a.Length; ++i)
-            {
-                result[i] = a[i] + b[i];
-            }
+            var va = new Vector<int>(a, i);
+            var vb = new Vector<int>(b, i);
+            (va + vb).CopyTo(result, i);
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Multiply(ref int[] a, ref int[] b, out int[] result)
+        for (; i < a.Length; ++i)
         {
-            result = new int[a.Length];
-            var simdLength = Vector<int>.Count;
-            int i;
-            for (i = 0; i <= a.Length - simdLength; i += simdLength)
-            {
-                var va = new Vector<int>(a, i);
-                var vb = new Vector<int>(b, i);
-                (va * vb).CopyTo(result, i);
-            }
-            for (; i < a.Length; ++i)
-            {
-                result[i] = a[i] * b[i];
-            }
+            result[i] = a[i] + b[i];
         }
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Multiply(ref int[] a, ref int[] b, out int[] result)
+    {
+        result = new int[a.Length];
+        var simdLength = Vector<int>.Count;
+        int i;
+        for (i = 0; i <= a.Length - simdLength; i += simdLength)
+        {
+            var va = new Vector<int>(a, i);
+            var vb = new Vector<int>(b, i);
+            (va * vb).CopyTo(result, i);
+        }
+        for (; i < a.Length; ++i)
+        {
+            result[i] = a[i] * b[i];
+        }
+    }
+}
