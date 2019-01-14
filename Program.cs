6bbde4f9197ad1b0b729645f08f7496717d6130a
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-01-14 12:44:36 +0100

    Adding multiplication test

--- Program.cs
+++ Program.cs
@@ -25,6 +25,24 @@ namespace heightmap_simd
                 result[i] = a[i] + b[i];
             }
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Multiply(ref int[] a, ref int[] b, out int[] result)
+        {
+            result = new int[a.Length];
+            var simdLength = Vector<int>.Count;
+            int i;
+            for (i = 0; i <= a.Length - simdLength; i += simdLength)
+            {
+                var va = new Vector<int>(a, i);
+                var vb = new Vector<int>(b, i);
+                (va * vb).CopyTo(result, i);
+            }
+            for (; i < a.Length; ++i)
+            {
+                result[i] = a[i] * b[i];
+            }
+        }
     }
 
     class Program
@@ -57,6 +75,7 @@ namespace heightmap_simd
 
         private static void Run8KImageTest()
         {
+            Console.WriteLine("Running addition test");
             var rnd = new Random();
             for (int x = 0; x < 10; x++)
             {
@@ -72,6 +91,26 @@ namespace heightmap_simd
                     for (int i = 0; i < a.Length; i++)
                     {
                         result2[i] = a[i] + b[i];
+                    }
+                });
+                Console.WriteLine($"[1] Elapsed = {sw} NO SIMD");
+            }
+
+            Console.WriteLine("Running multiplication test");
+                       for (int x = 0; x < 10; x++)
+            {
+                var a = Enumerable.Repeat(0, 4096 * 2160).Select(i => rnd.Next(0, 20)).ToArray();
+                var b = Enumerable.Repeat(0, 4096 * 2160).Select(i => rnd.Next(0, 20)).ToArray();
+
+                var sw = Measure(() => SIMD.Multiply(ref a, ref b, out int[] result));
+                Console.WriteLine($"[0] Elapsed = {sw} SIMD");
+
+                sw = Measure(() =>
+                {
+                    var result2 = new int[a.Length];
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        result2[i] = a[i] * b[i];
                     }
                 });
                 Console.WriteLine($"[1] Elapsed = {sw} NO SIMD");
