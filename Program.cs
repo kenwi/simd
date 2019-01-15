b7a8bac14bb2daddad590231c067e26e3c7ceab8
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-01-15 13:33:00 +0100

    Adding image write tests

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace heightmap_simd
             if (!Vector.IsHardwareAccelerated)
                 throw new Exception("No hw acceleration available.");
 
-            Console.WriteLine($"Run Time = {Measure(TestWrite, true)}" + Environment.NewLine);
+            Console.WriteLine($"Run Time = {Measure(Test1024x768Write, true)}" + Environment.NewLine);
+            Console.WriteLine($"Run Time = {Measure(Test8KWrite, true)}" + Environment.NewLine);
             Console.WriteLine($"Run Time = {Measure(TestCreateAndShowArray, true)}"+ Environment.NewLine);
             Console.WriteLine($"Run Time = {Measure(Test8KAddMultiply, true)}"+ Environment.NewLine);
         }
--- Tests.cs
+++ Tests.cs
@@ -11,15 +11,26 @@ namespace heightmap_simd
             if(!System.IO.Directory.Exists(directory))
                 System.IO.Directory.CreateDirectory(directory);
         }
-        private static void TestWrite()
+        private static void Test1024x768Write()
         {
             int width = 1024, height = 768;
             var rnd = new Random();
             var buffer = Enumerable.Repeat(0, width * height).Select(i => rnd.Next(0, 255)).ToArray();
-            var time = DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+            var time = DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture).Replace(":", "-");
 
             CreateDirectoryIfNotExists("./data");
             ImageWriter.Write(ref buffer, $"./data/TestWrite-{time}.png", width, height);
+        }
+        
+        private static void Test8KWrite()
+        {
+            int width = 7680, height = 4320;
+            var rnd = new Random();
+            var buffer = Enumerable.Repeat(0, width * height).Select(i => rnd.Next(0, 255)).ToArray();
+            var time = DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture).Replace(":", "-");
+
+            CreateDirectoryIfNotExists("./data");
+            ImageWriter.Write(ref buffer, $"./data/TestWrite8K-{time}.png", width, height);
         }
 
         private static void TestCreateAndShowArray()
