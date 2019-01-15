332a8a3f1b2894d62f8c7b0e6c6f6ea1ab31d3c9
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-01-15 18:44:07 +0100

    8k galaxy intensity image

--- Tests.cs
+++ Tests.cs
@@ -12,6 +12,38 @@ namespace heightmap_simd
             if (!System.IO.Directory.Exists(directory))
                 System.IO.Directory.CreateDirectory(directory);
         }
+
+        private static void TestIntensityImage()
+        {
+            int width = 7680, height = 4320;
+            var buffer = new Rgba32[width*height];
+
+            Func<double, double, double, double> getCentralIntensity = (r, i0, k) => {
+                return i0 * Math.Exp(-k * Math.Pow(r, 0.25));
+            };
+            Func<double, double, double, double> getOuterIntensity = (r, i0, a) => {
+                return i0 * Math.Exp(-r / a);
+            };
+            Func<double, double> getSurfaceBrightness = (r) => {
+                double i0 = 1.0, k = 0.02, a = 200;
+                double bulgeradius = (width + height) * 0.05;
+                return r < bulgeradius ? getCentralIntensity(r, i0, k) : getOuterIntensity(r-bulgeradius, getCentralIntensity(bulgeradius, i0, k), a);
+            };
+
+            for (int i = 0; i < width * height; i++)
+            {
+                var xy = ArrayIndex.From1DTo2D(i, width);
+                var distance = (new Vector2(xy[0], xy[1]) - new Vector2(width / 2, height / 2)).Length();                
+                var intensity = (float)Math.Clamp(getSurfaceBrightness(distance), 0, 1);
+                var color = new Rgba32(intensity, intensity, intensity); 
+                buffer[i] = color;
+            }
+
+            var time = DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture).Replace(":", "-");
+            CreateDirectoryIfNotExists("./data");
+            ImageWriter.FastWrite(ref buffer, $"./data/IntensityImage-{time}.png", width, height);
+        }
+
         private static void Test1024x768Write()
         {
             int width = 1024, height = 768;
