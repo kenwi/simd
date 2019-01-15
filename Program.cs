65f13c044d083361a246399a8cab7c49e2b30792
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-01-15 11:15:42 +0100

    Split tests to separate file. (yes I know about xunit blablah)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Numerics;
-using System.Linq;
 
 namespace heightmap_simd
 {
@@ -14,34 +13,5 @@ namespace heightmap_simd
             Console.WriteLine($"Run Time = {Measure(Run8KImageTest, true)}");
         }
 
-        private static void Run8KImageTest()
-        {
-            Console.WriteLine("SIMD Addition/Multiplication");
-            var rnd = new Random();
-            for (int x = 0; x < 10; x++)
-            {
-                var a = Enumerable.Repeat(0, 7680 * 4320).Select(i => rnd.Next(0, 20)).ToArray();
-                var b = Enumerable.Repeat(0, 7680 * 4320).Select(i => rnd.Next(0, 20)).ToArray();
-
-                var sw = Measure(() => SIMD.Add(ref a, ref b, out int[] result));
-                Console.WriteLine($"[+][SIMD] Elapsed = {sw}");
-
-                sw = Measure(() => SIMD.Multiply(ref a, ref b, out int[] result));
-                Console.WriteLine($"[*][SIMD] Elapsed = {sw}");
-            }
-
-            Console.WriteLine("NoSIMD Addition/Multiplication");
-            for (int x = 0; x < 10; x++)
-            {
-                var a = Enumerable.Repeat(0, 7680 * 4320).Select(i => rnd.Next(0, 20)).ToArray();
-                var b = Enumerable.Repeat(0, 7680 * 4320).Select(i => rnd.Next(0, 20)).ToArray();
-
-                var sw = Measure(() => NoSIMD.Add(ref a, ref b, out int[] result));
-                Console.WriteLine($"[+][NoSIMD] Elapsed = {sw}");
-
-                sw = Measure(() => NoSIMD.Multiply(ref a, ref b, out int[] result));
-                Console.WriteLine($"[*][NoSIMD] Elapsed = {sw}");
-            }
-        }
     }
 }
