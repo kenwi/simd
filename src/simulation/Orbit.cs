2379b653c688c058d532f1d1fe6c97d75b8dabf1
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-01-26 01:17:33 +0100

    Adding some velocity curves

--- src/simulation/Orbit.cs
+++ src/simulation/Orbit.cs
@@ -15,7 +15,7 @@ namespace simd
                     cosbeta = Math.Cos(beta),
                     sinbeta = Math.Sin(beta);
 
-            var pos = new Vector2(p.X + (float)(a * cosalpha * cosbeta - b * sinalpha * sinbeta), 
+            var pos = new Vector2(p.X + (float)(a * cosalpha * cosbeta - b * sinalpha * sinbeta),
                                     p.Y + (float)(a * cosalpha * sinbeta + b * sinalpha * cosbeta));
 
             if (pertubationAmplitude > 0 && numPertubations > 0)
@@ -24,6 +24,34 @@ namespace simd
                 pos.Y += (a / pertubationAmplitude) * (float)Math.Cos(alpha * 2 * numPertubations);
             }
             return pos;
+        }
+
+        // TODO:
+        public float CalculateOrbitalVelocity(float radius)
+        {
+            Func<double, double> MS = (r) =>
+            {
+                double d = 2000;
+                double rho_so = 1;
+                double rH = 2000;
+                return rho_so * Math.Exp(-r / rH) * (r * r) * Math.PI * d;
+            };
+
+            Func<double, double> MH = (r) =>
+            {
+                double rho_h0 = 0.15;
+                double rC = 2500;
+                return rho_h0 * 1 / (1 + Math.Pow(r / rC, 2)) * (4 * Math.PI * Math.Pow(r, 3) / 3);
+            };
+
+            Func<double, double> vd = (r) =>
+            {
+                double MZ = 1000;
+                double G = 6.672e-11;
+                return 20000 * Math.Sqrt(G * (MS(r) + MZ) / r);
+            };
+
+            return 0;
         }
     }
 }
