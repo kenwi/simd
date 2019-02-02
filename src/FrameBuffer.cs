46ca4d8c967683e67187124e6456138bfdb04e25
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-02-02 20:58:19 +0100

    Should be quicker

--- src/FrameBuffer.cs
+++ src/FrameBuffer.cs
@@ -1,4 +1,6 @@
 using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace simd
@@ -7,22 +9,23 @@ namespace simd
     {
         private int width;
         private int height;
-        private Rgba32[] frame;
+        private Image<Rgba32> frame;
 
         public FrameBuffer(int width, int height)
         {
             this.width = width;
             this.height = height;
-            frame = new Rgba32[width * height];
         }
 
         internal void MakeTestBuffer()
         {
-            for (int j = 0; j < height; j++)
+            frame = new Image<Rgba32>(width, height);
+            for (int y = 0; y < frame.Height; y++)
             {
-                for (int i = 0; i < width; i++)
+                var row = frame.GetPixelRowSpan(y);
+                for (int x = 0; x < frame.Width; x++)
                 {
-                    frame[i + j * width] = new Rgba32(j / (float)height, i / (float)width, 0.0f);
+                    row[x] = new Rgba32(y / (float)height, x / (float)width, 0.0f);
                 }
             }
         }
@@ -30,6 +33,7 @@ namespace simd
         internal void Write(string fileName)
         {
             ImageWriter.FastWrite(ref frame, fileName, width, height);
+            frame.Dispose();
         }
     }
 }
