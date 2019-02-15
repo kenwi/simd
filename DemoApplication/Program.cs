0a6417d6d5033f7b824a51daea8ec265654276ae
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-02-16 00:27:42 +0100

    Basic application

--- DemoApplication/Program.cs
+++ DemoApplication/Program.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using Veldrid;
+﻿using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
 
 namespace DemoApplication
-{ 
+{
     public sealed class Demo : Engine.Application
     {
         static void Main() => Demo.Instance.Run();
-
         static Demo Instance => _instance;
         static readonly Demo _instance = new Demo();
 
+        uint width = 1024, height = 768, viewScale = 1;
+        Sdl2Window window;
+
         protected override GraphicsDevice CreateGraphicsDevice()
         {
-            throw new NotImplementedException();
+            GraphicsDevice graphicsDevice;
+            GraphicsBackend backend = VeldridStartup.GetPlatformDefaultBackend();
+            
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                 new WindowCreateInfo(100, 100, (int)(width * viewScale), (int)(height * viewScale), WindowState.Normal, "Veldrid Ray Tracer"),
+                 new GraphicsDeviceOptions(debug: false, swapchainDepthFormat: null, syncToVerticalBlank: false),
+                 backend,
+                 out window,
+                 out graphicsDevice);
+
+            return graphicsDevice;
         }
     }
 }
---
+++ Engine/Application.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using Veldrid;
+
+namespace Engine
+{
+    public abstract class Application
+    {
+        public bool IsRunning { get; private set; }
+        public GraphicsDevice GraphicsDevice { get; private set; }
+        protected abstract GraphicsDevice CreateGraphicsDevice();
+
+        public void Run()
+        {
+            IsRunning = true;
+            GraphicsDevice = CreateGraphicsDevice();
+
+            const float dt = 0.1f;
+            while(IsRunning)
+            {
+                Update(dt);
+                Render(dt);
+            }
+        }
+
+        private void Render(float dt)
+        {
+            GraphicsDevice.SwapBuffers();
+            GraphicsDevice.WaitForIdle();
+        }
+
+        private void Update(float dt)
+        {
+
+        }
+    }
+}
