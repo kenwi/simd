876be58f76cdbf3445d6e549be2293369cb4f730
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-02-15 23:38:37 +0100

    Add application

---
+++ DemoApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Veldrid;
+
+namespace DemoApplication
+{ 
+    public sealed class Demo : Engine.Application
+    {
+        static void Main() => Demo.Instance.Run();
+
+        static Demo Instance => _instance;
+        static readonly Demo _instance = new Demo();
+
+        protected override GraphicsDevice CreateGraphicsDevice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
