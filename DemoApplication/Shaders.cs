0bfbfb5e932e2dd7102c9d8320b5e766c459027c
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-02-20 14:45:22 +0100

    Shading it up

--- DemoApplication/Shaders.cs
+++ DemoApplication/Shaders.cs
@@ -24,19 +24,27 @@ void main()
 layout(location = 0) in vec2 fsin_Position;
 layout(location = 1) in vec4 fsin_Color;
 layout(location = 0) out vec4 fsout_Color;
+const float epsilon = 0.5;
 
 float sdf(vec2 p)
 {
-    return length(p) - 1.0;
+    return length(p) - epsilon;
+}
+
+vec2 getNormal(vec2 p)
+{
+    float x = sdf(vec2(p.x + epsilon, p.y)) - sdf(vec2(p.x - epsilon, p.y));
+    float y = sdf(vec2(p.x, p.y + epsilon)) - sdf(vec2(p.x, p.y - epsilon));
+    return normalize(vec2(x, y));
 }
 
 void main()
 {
-    fsout_Color = fsin_Color;
-    
+    fsout_Color = vec4(0);
+    vec2 camera = vec2(1.0, 1.0);
     if(sdf(fsin_Position) < 0)
-    {
-        fsout_Color = fsin_Color * vec4(0.5);
+    {    
+        fsout_Color = fsin_Color * dot(getNormal(camera), fsin_Position);
     }
 }";
     }
