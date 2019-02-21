fc5c68d3d32efc1104a3a25a14bd26c9b54e3118
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-02-22 00:02:52 +0100

    ParticleDemo class

---
+++ DemoApplication/ParticleDemo.cs
@@ -0,0 +1,21 @@
+using Veldrid;
+namespace DemoApplication
+{
+    public sealed class ParticleDemo : Engine.Application
+    {
+        protected override GraphicsDevice CreateGraphicsDevice()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void CreateResources()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Update(double dt)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
