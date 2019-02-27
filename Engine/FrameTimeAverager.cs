2ca933f2b1583f89ab675dfcf0cf9a7d7a55a6ab
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-02-28 00:28:41 +0100

    Refactor

--- Engine/FrameTimeAverager.cs
+++ Engine/FrameTimeAverager.cs
@@ -2,47 +2,47 @@ namespace Engine
 {
     public class FrameTimeAverager
     {
-        private readonly double _decayRate = .3;
-        private readonly double _timeLimit;
+        private readonly double decayRate = .3;
+        private readonly double timeLimit;
 
-        private double _accumulatedTime;
-        private int _frameCount;
-        private int _totalFrames;
+        private double accumulatedTime;
+        private int frameCount;
+        private int totalFrames;
 
         public FrameTimeAverager(double maxTimeSeconds = 666)
         {
-            _timeLimit = maxTimeSeconds;
+            timeLimit = maxTimeSeconds;
         }
 
         public double CurrentAverageFrameTimeSeconds { get; private set; }
         public double CurrentAverageFrameTimeMilliseconds => CurrentAverageFrameTimeSeconds * 1000.0;
         public double CurrentAverageFramesPerSecond => 1 / CurrentAverageFrameTimeSeconds;
-        public int TotalFrames => _totalFrames;
+        public int TotalFrames => totalFrames;
 
         public void Reset()
         {
-            _accumulatedTime = 0;
-            _frameCount = 0;
+            accumulatedTime = 0;
+            frameCount = 0;
         }
 
         public void AddTime(double seconds)
         {
-            _accumulatedTime += seconds;
-            _frameCount++;
-            _totalFrames++;
-            if (_accumulatedTime >= _timeLimit) 
+            accumulatedTime += seconds;
+            frameCount++;
+            totalFrames++;
+            if (accumulatedTime >= timeLimit) 
                 Average();
         }
 
         private void Average()
         {
-            var total = _accumulatedTime;
+            var total = accumulatedTime;
             CurrentAverageFrameTimeSeconds =
-                CurrentAverageFrameTimeSeconds * _decayRate
-                + total / _frameCount * (1 - _decayRate);
+                CurrentAverageFrameTimeSeconds * decayRate
+                + total / frameCount * (1 - decayRate);
 
-            _accumulatedTime = 0;
-            _frameCount = 0;
+            accumulatedTime = 0;
+            frameCount = 0;
         }
     }
 }
